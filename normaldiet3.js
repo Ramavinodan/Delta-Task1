1182ec3b471cb5e73afa08c2fe3f9773b50ad255
Ramavinodan <49410013+Ramavinodan@users.noreply.github.com>
2019-05-26 03:52:11 +0530

    Update normaldiet3.js

--- normaldiet3.js
+++ normaldiet3.js
@@ -1,4 +1,4 @@
-var x,y,z,get1,get2,get3,get4;
+var x,y,z,get1,get2,get3,get4,a,b,c,d;
 function calculator1(weight){
 	
 	x=66+(6.23*weight);
@@ -16,22 +16,41 @@ function calculator(){
 }
 
 function getselect1(){
-	var get1=document.getElementById("morn").value;
+	get1=document.getElementById("morn").value;
 	document.getElementById("morning").innerHTML=get1;
+	 a=parseInt(get1);
 }
 
 function getselect2(){
-	var get2=document.getElementById("aftr").value;
+	get2=document.getElementById("aftr").value;
 	document.getElementById("afternoon").innerHTML=get2;
+	b=parseInt(get2);
 }
 
 
 function getselect3(){
-	var get3=document.getElementById("nght").value;
+	get3=document.getElementById("nght").value;
     document.getElementById("night").innerHTML=get3;
+    c=parseInt(get3);
 }
 function getselect4(){
-	
+	get4=a+b+c;
 	document.getElementById("overall").innerHTML=get4;
-
+    d=parseInt(get4);
+}
+function limit(dailyneed){
+	
+    var j=parseInt(dailyneed);
+    if(j>d)
+    {
+    	document.getElementById("empty").innerHTML="You are Exceeding the calorie limit";
+    }
+    if(j<d)
+    {
+    	document.getElementById("empty").innerHTML="You are at low calorie limit";
+    }
+    else
+    {
+    	document.getElementById("empty").innerHTML="You are at safe zone";
+    }
 }
